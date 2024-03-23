a6125f89da9123a5bb764ade2274a2f8bc534ecc
dennisjeanthompson <dennisjeanthompson@gmail.com>
2024-03-22 23:32:01 -0800

    images&quests

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,15 @@ namespace Engine.Models
         static ItemFactory() {
         
         _standardGameItems = new List<GameItem>();
-            _standardGameItems.Add(new Weapon(100, "Iron Sword", 2, "Forgeheart Blade", 1, 3));
-            _standardGameItems.Add(new Weapon(101, "Golden Sword", 6, "Sunfire Glimmer",1,5));
-            _standardGameItems.Add(new Weapon(102, "Pink Sword", 5, "Rose Blossom Azure ", 1, 3));
-            _standardGameItems.Add(new Weapon(103, "Blue Sword", 10, "Azure Frostblade", 1, 6));
+            _standardGameItems.Add(new Weapon(100, "Iron Sword", 2, "/ Engine; component / Images / ItemImage /RedSwordzx.png", 1, 3));
+            _standardGameItems.Add(new Weapon(100, "Iron Sword", 2, "/ Engine; component / Images / ItemImage /fireezzz.png", 1, 3));
+            _standardGameItems.Add(new Weapon(101, "Golden Sword", 6, "/ Engine; component / Images / ItemImage / sowrdssz.jpg", 1,5));
+            _standardGameItems.Add(new Weapon(102, "Pink Sword", 5, "/ Engine; component / Images / ItemImage / SwordReed.png", 1, 3));
+            _standardGameItems.Add(new Weapon(103, "Blue Sword", 10, "/ Engine; component / Images / ItemImage /glowing_sword_only__3045701784.png", 1, 6));
+            _standardGameItems.Add(new GameItem(104, "Potion", 15, "/ Engine; component / Images / ItemImage / Potionz.png"));
+            _standardGameItems.Add(new GameItem(106, "skulle", 4, "/ Engine; component / Images / ItemImage / wierd_small_evil_item__3517160176.png"));
+            _standardGameItems.Add(new GameItem(107, "Skullz", 10, "/ Engine; component / Images / ItemImage / wierd_small_evil_item_dead__1343190293.png"));
+
             // int itemTypeId,string name,int price,string imageName)
 
 
