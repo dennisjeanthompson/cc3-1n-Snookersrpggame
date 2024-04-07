f75a9cd791153eca286f67d2ba699815bef7bbbf
dennisjeanthompson <dennisjeanthompson@gmail.com>
2024-04-07 01:35:46 -0800

    actionButtonz

--- CC31N-SNOOKERS/MainWindow.xaml.cs
+++ CC31N-SNOOKERS/MainWindow.xaml.cs
@@ -79,10 +79,17 @@ namespace CC31N_SNOOKERS
                    </ DataGrid >
               </ TabItem >
         */
+        private void OnClick_AttackMonster(object sender, RoutedEventArgs e)
+        {
+            _gameSession.AttackCurrentMonster();
+        }
         private void OnGameMessageRaised(object sender, GameMessageEventArgs e)
         {
             GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Message)));
             GameMessages.ScrollToEnd();
         }
+
+      
+
     }
 }
